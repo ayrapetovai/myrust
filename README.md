55851dbe12d2526dd8a2b7924ee18555f9278350
artem <ayrapetovai@yandex.ru>
2021-04-19 15:18:18 +0300

    generics

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # myrust  
 
+Started on Apr 14, 2021
+
+The book https://doc.rust-lang.org/book  
+Another book https://stevedonovan.github.io/rust-gentle-intro/readme.html  
 Patterns https://github.com/rust-unofficial/patterns  
 Libraries list https://github.com/rust-unofficial/awesome-rust  
 Exercises https://github.com/rust-lang/rustlings  
---
+++ src/generics.rs
@@ -0,0 +1,66 @@
+use std::cmp::Ordering;
+
+macro_rules! compilation_error {
+    ($s:stmt $(;)?) => {}
+}
+
+#[allow(unused)]
+fn main() {
+    {
+        // generalizing code with type-parameter
+        fn largest<T: PartialOrd>(a: &[T]) -> &T {
+            let mut biggest_number = &a[0];
+            for v in a {
+                if biggest_number < v {
+                    biggest_number = v;
+                }
+            }
+            biggest_number
+        }
+
+        let v = vec![1, 6, 2, 9, 4, 3];
+        println!("biggest number in {:?} is {}", v, largest(&v));
+
+        #[derive(Debug)]
+        struct Vector {
+            x: i32,
+            y: i32,
+        }
+        impl PartialEq for Vector {
+            fn eq(&self, other: &Self) -> bool {
+                self.x == other.x && self.y == other.y
+            }
+        }
+        impl PartialOrd for Vector {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                Option::from((self.x * self.y).cmp(&(other.x * other.y)))
+            }
+        }
+
+        let vectors = vec![Vector{ x: 1, y: 1 }, Vector{ x: 7, y: 2 }, Vector{ x: 2, y: 5 }];
+        println!("vectors {:?} biggest is {:?}", vectors, largest(&vectors))
+    }
+    {
+        // Generics arguments are a part of a type
+        struct Point<X, Y> {
+            x: X,
+            y: Y,
+        }
+        // in method definitions
+        // impl <T> is needed for compiler to understand, that Point<T>'s 'T' is not a concrete type name, but a type-parameter name
+        impl<T, Y> Point<T, Y> {
+            fn x(&self) -> &T {
+                &self.x
+            }
+        }
+
+        let f_i_point = Point { x: 1.0, y: 3 };
+        let mut i_f_point = Point { x: 42, y: 3.14 };
+        compilation_error!(
+            i_f_point = f_i_point; // mismatched types
+        );
+        i_f_point = Point { x: 1, y: 0.23 };
+
+        println!("x part of point is {}", i_f_point.x());
+    }
+}
