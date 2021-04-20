92b2772c453fcf47105f07bb4da3439f3907a86c
artem <ayrapetovai@yandex.ru>
2021-04-20 14:33:24 +0300

    add move/borrow for 'match'

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -1,4 +1,25 @@
 // TODO match
+extern crate myrust;
+use myrust::compilation_error;
+
+#[derive(Debug)]
+struct Verbose {
+    id: i32
+}
+
+impl Verbose {
+    fn new(new_id: i32) -> Verbose {
+        Verbose { id: new_id }
+    }
+}
+
+impl Drop for Verbose {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
+
+#[allow(unused)]
 fn main() {
     {
         let some_u8_value = Some(0u8);
@@ -31,5 +52,32 @@ fn main() {
         }
         test_and_print_plus_one(returns_none_or_number(true));
         test_and_print_plus_one(returns_none_or_number(false));
+    }
+    {
+        // 'match' takes ownership, only one value will be created
+        println!("A println before match");
+        let v = match Verbose::new(1) { //                <--|
+            // match pattern's type corresponds to the type of expression ---|
+            inner_variable => { // : Verbose; move value to 'inner_variable', because type of `match`'s expression is not a reference
+                println!("This is println from 'match' {:?}", inner_variable);
+                inner_variable
+            }
+        };
+        println!("A println after match, {:?}", v);
+        // value will be dropped here
+    }
+    {
+        // 'match' takes ownership, only one value will be created
+        println!("A println before match");
+        let v = match &Verbose::new(2) { // creates a temporary which is freed while still in use
+            inner_variable => { // : &Verbose; borrow value, because type of `match`'s expression is a reference
+                println!("This is println from 'match' {:?}", inner_variable);
+                inner_variable // is valid in 'match', because 'match ... {}' is a one expression
+            }
+        };// value will be dropped here
+        compilation_error!(
+            println!("A println after match, {:?}", v); // temporary value dropped while borrowed
+        );
+        println!("A println after match")
     }
 }
